280c2cc29d43e5aa235e3d3b3d4116b5c31b4797
tania2379 <tania2308011@hybridge.education>
2024-08-25 21:54:27 -0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'postgresql://username:password@localhost/mi_flask_app'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # Puedes añadir más configuraciones aquí si es necesario
